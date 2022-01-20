d4d5c01c923cb16917b176a6d77c12ce2bbe2b39
rab97 <s290362@studenti.polito.it>
2022-01-20 10:18:10 +0100

    fix: coloring coloring vertex indexes

--- algorithms/Luby.cpp
+++ algorithms/Luby.cpp
@@ -2,8 +2,6 @@
 // Created by marta on 15/01/22.
 //
 #include "Luby.h"
-
-#include <iostream>
 #include <condition_variable>
 
 mutex I_mutex, remainingNode_mutex;
@@ -22,20 +20,20 @@ void Luby::algorithmSolver(Graph &graph) {
     std::set<int> I;
 
     int num_vertices = graph.num_vertices();
-    int max_color= 0;
+    int max_color = 0;
     //graph.addColor(max_color);
-    int running_threads= num_threads;
+    int running_threads = num_threads;
 
-    int num_remain= graph.num_vertices();
+    int num_remain = graph.num_vertices();
     std::set<int> nodes_remain;
     std::vector <Vertex> *verticesList = graph.getVerticesList();
-    for(int i=0; i< num_remain; i++) {
-        nodes_remain.insert(verticesList->at(i).getId());
+    for (int i = 0; i < num_remain; i++) {
+        nodes_remain.insert(i);
     }
 
 
-    vector<int> assigned_vertices(num_vertices+1, 0);
-    semMIS= num_threads;
+    vector<int> assigned_vertices(num_vertices, 0);
+    semMIS = num_threads;
 
     if (num_threads > std::thread::hardware_concurrency())
         throw std::runtime_error("Hardware concurrency exceeded: please use at most " +
@@ -46,18 +44,16 @@ void Luby::algorithmSolver(Graph &graph) {
 
     //Part 1 : assign all vertices a random permutation and then find all MIS in the graph
     for (int i = 0; i < num_threads; i++) {
-        threads.emplace_back(std::thread([&graph, &mis, &I, &assigned_vertices, &max_color, &running_threads,this, s, i]() {
-            int from = s.get_min(i), to = s.get_max(i);
+        threads.emplace_back(
+                std::thread([&graph, &mis, &I, &assigned_vertices, &max_color, &running_threads, this, s, i]() {
+                    int from = s.get_min(i), to = s.get_max(i);
 
-            assign_num_to_vertices(from, to, assigned_vertices, graph);
-            find_MIS_Parallel(from, to, max_color, running_threads,mis, I, assigned_vertices, graph);
-            std::cout << "arrivo qui0?" << endl;
+                    assign_num_to_vertices(from, to, assigned_vertices, graph);
+                    find_MIS_Parallel(from, to, max_color, running_threads, mis, I, assigned_vertices, graph);
 
-        }));
-        std::cout << "arrivo qui1?" << endl;
+                }));
     }
 
-    std::cout << "arrivo qui?  2" << endl;
     for (auto &thread: threads) {
         thread.join();
     }
@@ -113,42 +109,38 @@ void Luby::algorithmSolver(Graph &graph) {
 }
 
 
-
-
-
-
-void Luby::find_MIS_Parallel(int from, int to, int &max_color,int &running_threads,std::set <std::set<int>> &mis, std::set<int> &I,
-                                                 vector<int> &assigned_vertices, Graph &graph) {
+void Luby::find_MIS_Parallel(int from, int to, int &max_color, int &running_threads, std::set <std::set<int>> &mis,
+                             std::set<int> &I,
+                             vector<int> &assigned_vertices, Graph &graph) {
 
     int num_remain = to - from;
     std::set<int> nodes_remain;
     std::set<int> neighbors;
     std::vector <Vertex> *verticesList = graph.getVerticesList();
-    running_threads= num_threads;
+    running_threads = num_threads;
 
-    for(int i=from; i< to; i++) {
-        nodes_remain.insert(verticesList->at(i).getId());
+    for (int i = from; i < to; i++) {
+        nodes_remain.emplace(i);
     }
 
     int cur_node_id;
     std::vector <Vertex> neighborList;
 
-    while (num_remain >0) {
+    while (num_remain > 0) {
 
         for (int j = from; j < to; j++) {
 
-            cur_node_id=  verticesList->at(j).getId();
+            cur_node_id = j;
 
-            if (nodes_remain.find(cur_node_id) != nodes_remain.end()) {
+            if (nodes_remain.find(j) != nodes_remain.end()) {
 
-                bool isMax= isMax_between_neighbor(verticesList->at(j), assigned_vertices);
-                if(isMax){
+                bool isMax = isMax_between_neighbor(verticesList->at(j), assigned_vertices);
+                if (isMax) {
                     //vertex is colored and inserted into the indipendent set
                     I_mutex.lock();
                     I.insert(cur_node_id);
                     //graph.colorVertex(&verticesList->at(j), max_color);
-                    std::cout << "cur_node_id= " << cur_node_id << "    color= " << max_color << endl;
-                    assigned_vertices.at(cur_node_id)= -1;       //the next iteration it won't be the max
+                    assigned_vertices[cur_node_id] = -1;       //the next iteration it won't be the max
                     I_mutex.unlock();
 
                     //remove neighbors from nodes_remain if they belong to the interval [from, to)
@@ -156,21 +148,21 @@ void Luby::find_MIS_Parallel(int from, int to, int &max_color,int &running_threa
                     int neighbor_id;
                     for (int k = 0; k < neighborList.size(); k++) { //for each neighbor of that node
 
-                        neighbor_id = neighborList.at(k).getId();
+                        neighbor_id = neighborList[k].getId() - 1;
 
 
-                            if (nodes_remain.find(neighbor_id) != nodes_remain.end()) {
-                                num_remain--;
-                                neighbors.insert(neighbor_id);
-                                nodes_remain.erase(neighbor_id);
-                            }
+                        if (nodes_remain.find(neighbor_id) != nodes_remain.end()) {
+                            num_remain--;
+                            neighbors.insert(neighbor_id);
+                            nodes_remain.erase(neighbor_id);
+                        }
                     }
 
                     //remove cur_node_id from nodes_remain
                     num_remain--;
                     nodes_remain.erase(cur_node_id);
 
-                }else{
+                } else {
                     neighbors.insert(cur_node_id);
                 }
 
@@ -186,20 +178,11 @@ void Luby::find_MIS_Parallel(int from, int to, int &max_color,int &running_threa
         if (semMIS <= 0) {
             //Insertion of indipendent set into mis
             mis.insert(I);
-
-            auto it= I.begin();
-            while(it!= I.end()){
-
-                std::cout << "I= " << max_color << "        node_in_I= " << *it << endl;
-                it++;
-            }
-
-            max_color++;
             graph.addColor(max_color);
-            std::cout << "max_color=" << max_color<< endl;
+            max_color++;
 
             I.clear();
-            if(num_remain==0){
+            if (num_remain == 0) {
                 running_threads--;
             }
             semMIS = running_threads;
@@ -207,33 +190,21 @@ void Luby::find_MIS_Parallel(int from, int to, int &max_color,int &running_threa
 
         } else {
 
-            if(num_remain==0){
+            if (num_remain == 0) {
                 running_threads--;
             }
-            std::cout << "Un thread in attesa" << endl;
             mis_cond_var.wait(lock);
         }
 
         num_remain = neighbors.size();
         nodes_remain = neighbors;
         neighbors.clear();
-        std::cout << "RANGE [ " << from << ", " << to << "]     nodes_remain= " << num_remain << endl;
-        std::cout << "semMIS=" << semMIS << endl;
-
     }
-
-
-    std::cout << "here"<< endl;
-
-}
-
-
 }
 
 
 void Luby::assign_num_to_vertices(int from, int to, std::vector<int> &assigned_vertices, Graph &graph) {
 
-    std::vector <Vertex> vertices = *graph.getVerticesList();
     //permutation of id numbers
     srand(time(0));
     //std::shuffle(vertices.begin(), vertices.end(), 123);
@@ -247,20 +218,20 @@ void Luby::assign_num_to_vertices(int from, int to, std::vector<int> &assigned_v
 bool Luby::isMax_between_neighbor(Vertex v, std::vector<int> &assigned_vertices) {
 
     std::vector <Vertex> neighbor = v.getNeighborList();
-    int value = assigned_vertices[v.getId()];
+    int value = assigned_vertices[v.getId() - 1];
     int neighbor_id;
 
     for (int i = 0; i < neighbor.size(); i++) {
 
-        neighbor_id = neighbor.at(i).getId();
+        neighbor_id = neighbor[i].getId() - 1;
 
-        if (assigned_vertices.at(neighbor_id) > value) {
+        if (assigned_vertices[neighbor_id] > value) {
             //found a neighbor with higher value
             return false;
 
-        } else if (assigned_vertices.at(neighbor_id) == value) {
+        } else if (assigned_vertices[neighbor_id] == value) {
             //if they have the same random value look at the id
-            if (neighbor_id > v.getId()) {
+            if (neighbor_id + 1 > v.getId()) {
                 return false;
             }
         }
@@ -297,5 +268,5 @@ void Luby::color_MIS(std::set<int> I, int color, Graph &graph) {
 
 
 std::string Luby::name() const {
-    return "Luby (" + std::to_string(num_threads) + " threads),";
+    return "Luby (" + std::to_string(num_threads) + " threads)";
 };
--- resolve/Resolve.cpp
+++ resolve/Resolve.cpp
@@ -16,12 +16,11 @@
 
 Resolve::Resolve(Graph &g) : algorithms(
         {new Sequential(),
-         new MIS_Sequential(),
+                // new MIS_Sequential(),
          new LDF(1), new LDF(2), new LDF(4), new LDF(8),
          new SDL(1), new SDL(2), new SDL(4), new SDL(8),
-         new Luby(1), new Luby(2), new Luby(4), new Luby(8)
-                //new JP(1), new JP(2), //new JP(4), new JP(8)
-
+         new JP(1), new JP(2), new JP(4), new JP(8),
+         new Luby(1), new Luby(2), new Luby(4), new Luby(8),
         }), graph(g) {}
 
 Resolve::Resolve(Graph &g, std::string alg) : graph(g) {
@@ -60,7 +59,7 @@ void Resolve::resolve_all(std::string path) {
         struct result res = run_one(a);
         out.write_line(res, a);
         std::cout << "algorithm_name: " << a->name();
-        printf(", time_elapsed: %.2f ms, mem_usage: %.2f kB, num_colors: %d \n",
+        printf(", time_elapsed: %.2f ms, mem_usage: %.2f MB, num_colors: %d \n",
                res.milliseconds,
                res.mem_usage,
                res.num_colors);
@@ -76,7 +75,7 @@ struct result Resolve::run_one(Algorithm *algorithm) {
     auto t1 = std::chrono::high_resolution_clock::now();
     algorithm->algorithmSolver(graph);
     auto t2 = std::chrono::high_resolution_clock::now();
-    double mem_usage = memory_usage.stop() / 1024;
+    double mem_usage = memory_usage.stop() / 1024 / 1024;
 
     int num_colors = graph.getNumColors();
     double milliseconds = std::chrono::duration<double, std::milli>(t2 - t1).count();
